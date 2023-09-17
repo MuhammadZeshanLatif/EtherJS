82f1d4d2d8efb0a0b22b38e083a5c78ccd18f1ed
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-09-18 02:52:29 +0500

    Ether

--- readBlockChain.js
+++ readBlockChain.js
@@ -1,28 +1,34 @@
-// const { ethers } = require("ethers");
-
-// const provider = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/d6b7c4625a424f51a9d2c7a45d6024b1");
-
-// const queryBlockchain = async () => {
-//     const block = await provider.getBlockNumber();
-//     console.log("Current Block Number:", block);
+// const {ethers}=require("ethers");
+// const providers = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/d6b7c4625a424f51a9d2c7a45d6024b1");
+// //GetCurrent Block
+// const getCurrentBlockNumber=async ()=>{
+//     const block=await providers.getBlockNumber();
+//     console.log("The Current Block Number is :",block);
 // }
+//  getCurrentBlockNumber();
 
-// queryBlockchain(); // Call the function to execute it
+// const {ethers}=require("ethers");
+// const providers=new ethers.JsonRpcProvider("https://sepolia.infura.io/v3/d6b7c4625a424f51a9d2c7a45d6024b1");
+// const getBalance=async ()=>{
+//     const balance= await providers.getBalance("0xcE2a59bB722c17c555D52dcD8ea40E32e1E6Cd55");
+//     console.log("Blance is ",ethers.formatEther(balance),"ETH");
+// }
+// getBalance();
 
-const { ethers } = require("ethers");
+// const { ethers } = require("ethers");
+// const providers = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/d6b7c4625a424f51a9d2c7a45d6024b1");
+// const networkInfo = async () => {
+//     try {
 
-const provider = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/d6b7c4625a424f51a9d2c7a45d6024b1");
+//         const _networkInfo = await providers.getNetwork();
+//         console.log(_networkInfo.toJSON());
+//         console.log(_networkInfo.name)
+//         console.log(_networkInfo.chainId)
+//     } catch (e) {
+//         console.log(e);
+//     }
 
-const queryBlockchain = async () => {
-    const address = "0xcE2a59bB722c17c555D52dcD8ea40E32e1E6Cd55"; // Replace with the address you want to check
+// }
+// networkInfo();
 
-    try {
-        const balance = await provider.getBalance(address);
-        console.log("Balance:", balance, "ETH");
-    } catch (error) {
-        console.error("Error fetching balance:", error);
-    }
-};
-
-queryBlockchain(); // Call the function to execute it
 
