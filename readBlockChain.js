d57d92d1b630b2207fe54a359ec49fc64e21a71f
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-09-18 00:03:48 +0500

    now

--- readBlockChain.js
+++ readBlockChain.js
@@ -1,2 +1,10 @@
-//firstly we need to import ether library
-const {ether} = require('ether');
+const { ethers } = require("ethers");
+
+const provider = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/d6b7c4625a424f51a9d2c7a45d6024b1");
+
+const queryBlockchain = async () => {
+    const block = await provider.getBlockNumber();
+    console.log("Current Block Number:", block);
+}
+
+queryBlockchain(); // Call the function to execute it
