96cc4ccd0b52c3f6ab296810c2bd00a535ebc629
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-09-18 00:11:52 +0500

    commentr

--- readBlockChain.js
+++ readBlockChain.js
@@ -1,10 +1,28 @@
+// const { ethers } = require("ethers");
+
+// const provider = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/d6b7c4625a424f51a9d2c7a45d6024b1");
+
+// const queryBlockchain = async () => {
+//     const block = await provider.getBlockNumber();
+//     console.log("Current Block Number:", block);
+// }
+
+// queryBlockchain(); // Call the function to execute it
+
 const { ethers } = require("ethers");
 
 const provider = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/d6b7c4625a424f51a9d2c7a45d6024b1");
 
 const queryBlockchain = async () => {
-    const block = await provider.getBlockNumber();
-    console.log("Current Block Number:", block);
-}
+    const address = "0xcE2a59bB722c17c555D52dcD8ea40E32e1E6Cd55"; // Replace with the address you want to check
+
+    try {
+        const balance = await provider.getBalance(address);
+        console.log("Balance:", balance, "ETH");
+    } catch (error) {
+        console.error("Error fetching balance:", error);
+    }
+};
 
 queryBlockchain(); // Call the function to execute it
+
